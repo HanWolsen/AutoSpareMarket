93225b7c3fffc72816df47fbdb6683ff6101f4be
OxYxO <ayub.arbiev@gmail.com>
2025-11-06 22:42:03 +0300

    delete usings

--- AutoSpareMarket.DAL/SqlServer/Configuration/OrderConfiguration.cs
+++ AutoSpareMarket.DAL/SqlServer/Configuration/OrderConfiguration.cs
@@ -1,12 +1,6 @@
 ﻿using AutoSpareMarket.Domain.Models.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AutoSpareMarket.DAL.SqlServer.Configuration
 {
--- AutoSpareMarket.DAL/SqlServer/Configuration/SupplierProductConfiguration.cs
+++ AutoSpareMarket.DAL/SqlServer/Configuration/SupplierProductConfiguration.cs
@@ -1,15 +1,10 @@
 ﻿using AutoSpareMarket.Domain.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AutoSpareMarket.DAL.SqlServer.Configuration
 {
-    internal class SupplierProductConfiguration : IEntityTypeConfiguration<SupplierProduct> 
+    internal class SupplierProductConfiguration : IEntityTypeConfiguration<SupplierProduct>
     {
         public void Configure(EntityTypeBuilder<SupplierProduct> builder)
         {
