02cdde025acfe567548b7d19fb8f396103bcc5cc
Max <mechnin.maxem@yandex.ru>
2025-11-23 21:33:26 +0500

    Rename folders and Add AutoSpareMarket.Service

---
+++ AutoSpareMarket.Service/Service/Implementations/BaseService.cs
@@ -0,0 +1,11 @@
+﻿using AutoSpareMarket.Domain.Models.Abstractions;
+using AutoSpareMarket.Service.Service.Intarfaces;
+
+namespace AutoSpareMarket.Service.Service.Implementations
+{
+    public class BaseService<T> : IBaseService<T>
+        where T : BaseEntity
+    {
+       //protected readonly BaseRepository 
+    }
+}
---
+++ AutoSpareMarket.Service/Service/Intarfaces/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using AutoSpareMarket.Domain.Models.Abstractions;
+
+namespace AutoSpareMarket.Service.Service.Intarfaces
+{
+    public interface IBaseService<T>
+        where T : BaseEntity
+    {
+
+    }
+}
