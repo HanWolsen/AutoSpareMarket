7aa5c850282428bf3a932f212c98cc788706a67e
OxYxO <ayub.arbiev@gmail.com>
2025-11-23 19:41:00 +0300

    fix services includ

--- AutoSpareMarket.Service/Service/Implementations/BaseService.cs
+++ AutoSpareMarket.Service/Service/Implementations/BaseService.cs
@@ -1,11 +1,18 @@
-﻿using AutoSpareMarket.Domain.Models.Abstractions;
+﻿using AutoSpareMarket.DAL.Repository.Implementations;
+using AutoSpareMarket.DAL.Repository.Intarfacec;
+using AutoSpareMarket.Domain.Models.Abstractions;
 using AutoSpareMarket.Service.Service.Intarfaces;
 
 namespace AutoSpareMarket.Service.Service.Implementations
 {
     public class BaseService<T> : IBaseService<T>
-        where T : BaseEntity
+        where T : BaseEntity    
     {
-       //protected readonly BaseRepository 
+        protected readonly IBaseRepository<T> _baseRepository;
+
+        public BaseService(IBaseRepository<T> baseRepository)
+        {
+            _baseRepository = baseRepository;
+        }
     }
 }
